5de4fcf08cb0573bee4328f9783b2720e8ed8eaf
Furkan Kaymaz <furkannkaymaz@gmail.com>
2023-07-14 00:28:08 +0300

    ConventerExtensions.kt added

--- utils/src/main/java/com/furkan/utils/conventer/DataConverter.kt
+++ utils/src/main/java/com/furkan/utils/conventer/DataConverter.kt
@@ -1,4 +1,4 @@
-package com.furkan.utils
+package com.furkan.utils.conventer
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
--- utils/src/main/java/com/furkan/utils/conventer/toJson.kt
+++ utils/src/main/java/com/furkan/utils/conventer/toJson.kt
@@ -1,26 +1,11 @@
-package com.furkan.utils.extensions
-
-import com.furkan.utils.conventer.DataConverter
+package com.furkan.utils.conventer
 
 fun Any?.toJson(): String = DataConverter.toJson(this)
 
-/**
- *  Convert a String to an Object
- */
 inline fun <reified T> String?.toObject(): T = DataConverter.fromJson(this)
 
-/**
- * Convert a Map to an Object
- */
 inline fun <reified T> Map<String, Any>.toObject(): T = convert()
 
-/**
- * Convert an object to a Map
- */
 inline fun <reified T> T.toMap(): Map<String, Any> = convert()
 
-/**
- * Convert an object of type T to type R
- */
 inline fun <reified T, reified R> T.convert(): R = DataConverter.toJson(this).toObject()
-//inline fun <T, reified R> T.convert(): R =  gSon.toJson(this).toObject()
